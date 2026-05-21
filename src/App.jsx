f74e1d1af6d1a3112f12cd04003d7c2ed83de628
manojponnusamy2032-star <manojponnusamy2032@gmail.com>
2026-05-21 13:42:10 +0530

    fix: remove all leftover Vite template code from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -38,34 +38,4 @@ function App() {
               <a href="https://x.com/vite_js" target="_blank">
                 <svg
                   className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
-}
-
-export default App
